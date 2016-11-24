441788569c957a6319187a5604176b43d79156af
Jeetestu <j_turnock@hotmail.com>
2016-11-24 10:45:03 -0500

    Add files via upload

---
+++ UseLedAndDetectRed.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+import RPi.GPIO as GPIO
+import time
+cap = cv2.VideoCapture(0)
+#Red is a unique color to detect because it has both a high and low color values (it is the upper and lower range)
+#To detect red, this code creates 2 masks and combines them
+#The resulting mask shows all parts of the video that match the red threshold in white
+#The filtered image is the result of a bitwiseAND operation on the mask and the regular video
+#The filtered image is also blurred to reduce noise. 
+#Also turns on the laser
+
+laserLinePin = 18
+#GPIO.BCM and GPIO.BOARD indicate different pin numbering conventions depending on the Raspberry Pi version
+GPIO.setmode(GPIO.BCM) 
+#Disables warnings
+GPIO.setwarnings(False)
+#Sets the pin as an output pin
+GPIO.setup(laserLinePin, GPIO.OUT)
+#Turn pin on (default is 3.3V)
+GPIO.output(laserLinePin, GPIO.HIGH)
+
+while(1):
+    # Take each frame
+    _, frame = cap.read()
+    # Convert BGR to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # lower mask (0-10) for lower threshold of red
+    lower_red = np.array([0,50,50])
+    upper_red = np.array([10,255,255])
+    mask0 = cv2.inRange(hsv, lower_red, upper_red)
+
+    # upper mask (170-180) for upper thresholdof red
+    lower_red = np.array([170,50,50])
+    upper_red = np.array([180,255,255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+
+    # combine 
+    mask = mask0+mask1
+    
+    # Bitwise-AND mask and original image
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+	#The blurred image to reduce noise (higher number = more blurred; Must be odd number)
+    blurred = cv2.medianBlur(res,7)
+	
+    cv2.imshow('blurred',blurred)
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
+        break
+cv2.destroyAllWindows()
+
