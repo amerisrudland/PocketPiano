3fbffc5c33769c51f0df41bff16cc6578b59e8e0
Jeetestu <j_turnock@hotmail.com>
2016-12-07 13:14:00 -0500

    Working Code - Cleaned up
    Cleaned up extraneous comments and added user ability to toggle various displays (framerate, windows etc.)

--- MainCode.py
+++ MainCode.py
@@ -8,6 +8,8 @@ import pygame
 import csv
 import pickle
 
+startTime=time.time()
+
 #define colors
 white = (255,255,255)
 teal = (161,232,9)
@@ -37,7 +39,7 @@ time.sleep(0.1)
 def nothing(x):
     pass
 # Creating a window for later use
-cv2.namedWindow('HSV')
+cv2.namedWindow('result')
 
 # Starting with 100's to prevent error while masking
 h,s,v = 137,58,137
@@ -45,9 +47,9 @@ h,s,v = 137,58,137
 # Creating track bar Prev: 142,64,161
 # the first integer parameter are the starting values
 
-cv2.createTrackbar('h', 'HSV',137,179,nothing)
-cv2.createTrackbar('s', 'HSV',58,255,nothing)
-cv2.createTrackbar('v', 'HSV',137,255,nothing)
+cv2.createTrackbar('h', 'result',137,179,nothing)
+cv2.createTrackbar('s', 'result',58,255,nothing)
+cv2.createTrackbar('v', 'result',137,255,nothing)
 
 #SETUP BLOB DETECTION
 #http://www.learnopencv.com/blob-detection-using-opencv-python-c/
@@ -94,56 +96,55 @@ for note in notes:
 
 #Get bounding rectangle around all notes
 pianoX,pianoY,pianoWidth,pianoHeight = cv2.boundingRect(np.array(allNotePoints))
-print'piano coordinate (top left) : {0},{1}'.format(pianoX,pianoY)
 
+#Because we are now taking a subsection of the frame, we need to offset all note regions for the new
+#co-ordinate system
 offsetNotes = []
 offsetNote = []
-print'old notes:{0}'.format(notes)
 for note in notes:
     offsetNote[:] = []
     for point in note:
-        print'original note: {0}'.format(point)
-        print'new note:{0}'.format(point[0]-pianoX,point[1]-pianoY)
         offsetNote.append((point[0]-pianoX,point[1]-pianoY))
     offsetNotes.append(list(offsetNote))
 
 notes = offsetNotes
-print'new notes:{0}'.format(notes)
-
-#x=allNotePoints[0][0]
-#y=allNotePoints[0][1]
-#xOffset=x-pianoX
-#yOffset=y-pianoY
-#print'original point coordinate: {0},{1}'.format(x,y)
-#print'original point coordinate: {0},{1}'.format(xOffset,yOffset)
-
-#creating masks from calibration data
-noteRegions = []
-for idx,note in enumerate(notes):
-    noteRegions.append(np.zeros((480,640),np.uint8))
-    cv2.fillPoly(noteRegions[idx], [np.int32(note)],white)
-
 
 #IMPORTANT
 #OPEN CV uses BGR instead of RGB
 #OPEN CV HSV values range from H = 0-180, S = 0-255 and V = 0-255. Scale accordingly
 
+print'Time to start up: {0} seconds'.format(time.time() - startTime)
+
+#Boolean values to indicate what operations to conduct
+showRes=True
+showWholeFrame=False
+showFrame=True
+showMask=False
+showFrameRate = False
+
+
 #MAIN LOOP
 
 for frameRaw in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
-    frame = frameRaw.array
+    if showFrameRate == True:
+        startTime = time.time()
+
+    wholeFrame = frameRaw.array
     #get region of interest using bounding rect (just the part of the frame with piano
-    frame = frame[pianoY:pianoY+pianoHeight,pianoX:pianoX+pianoWidth]
+    frame = wholeFrame[pianoY:pianoY+pianoHeight,pianoX:pianoX+pianoWidth]
     
     #draw note regions in frame
     for note in notes:
         cv2.polylines(frame, [np.int32(note)],1,teal)
         # Convert BGR to HSV
     hsv= cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # get info from track bar and appy to result
-    h = cv2.getTrackbarPos('h','HSV')
-    s = cv2.getTrackbarPos('s','HSV')
-    v = cv2.getTrackbarPos('v','HSV')
+
+    if showRes == True:
+        # get info from track bar and appy to result
+        h = cv2.getTrackbarPos('h','result')
+        s = cv2.getTrackbarPos('s','result')
+        v = cv2.getTrackbarPos('v','result')
+        
 
     #creates a mask with a lower threshold using the hsv values
     #and upper threshold of 180,255,255
@@ -152,37 +153,18 @@ for frameRaw in camera.capture_continuous(rawCapture, format="bgr",use_video_por
     mask = cv2.inRange(hsv, lowerRange, upperRange)
     
     # Bitwise-AND mask and original image
-    result = cv2.bitwise_and(frame,frame, mask= mask)
+    if showRes == True:
+        result = cv2.bitwise_and(frame,frame, mask= mask)
+        cv2.imshow('result',result)
+        
     #The blurred image to reduce noise (higher number = more blurred; Must be odd number)
-    blurredMask = cv2.medianBlur(mask,7)
-    blurred = cv2.medianBlur(result,7)
+    #blurredMask = cv2.medianBlur(mask,7)
 
     keypoints = []
 
-    #OPTIMIZE THIS LOOP
-    #print 'region size: {0}'.format(noteRegions[0].shape)
-    #print 'blurredMask size: {0}'.format(blurredMask.shape)
-    #for region in noteRegions:
-    #   searchArea = cv2.bitwise_and(blurredMask,blurredMask,mask = region)
-    #   keypoints.append(detector.detect(searchArea))
-    # We want to search just the region not the whole picture to speed up
-    # keypoints.append(detector.detect(blurredMask,region)) # Maybe something like this?
-
     #Get all keypoints in the image and print
-    keypoints = detector.detect(blurredMask)
-    frame= cv2.drawKeypoints(frame,keypoints,np.array([]), (0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    #Draw detected blobs as red circles
-    #frameWithKeypoints = cv2.drawKeypoints(frame,keypoints,np.array([]), (0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
- 
-
-    #Check if regions are detecting keypoints
-   # for idx,keys in enumerate(keypoints):
-   #     if keys:
-   #         frame=cv2.drawKeypoints(frame,keys,np.array([]), teal, cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-   #         print 'keypoint detected in region {0}'.format(idx)
-
-
+    keypoints = detector.detect(mask)
+    
     #check all noteRegions for keypoints
     #for idx,region in enumerate(notes):
     for idx,region in enumerate(notes):
@@ -190,19 +172,35 @@ for frameRaw in camera.capture_continuous(rawCapture, format="bgr",use_video_por
            if cv2.pointPolygonTest(np.array(region),key.pt,False)  == 1.0:
                 print 'Region {0} is triggered'.format(idx)
     
-    #show images (i.e the frames
-    #cv2.imshow("Keypoints", frameWithKeypoints)
-    cv2.imshow('de-noised',blurred)
-    cv2.imshow('frame',frame)
-    cv2.imshow('mask',mask)
-    cv2.imshow('region0',noteRegions[0])
-    cv2.imshow('result',result)
+    #show images (i.e the frames)
+    if showFrame == True:
+        frame= cv2.drawKeypoints(frame,keypoints,np.array([]), (0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        cv2.imshow('piano',frame)
+    if showWholeFrame == True:
+        cv2.imshow('frame',wholeFrame)
+    if showMask == True:
+        cv2.imshow('mask',mask)
+    
+    #Check for keyboard press and assign key pressed (if any) to k
     k = cv2.waitKey(1) & 0xFF
-    if k == 27:
+    if k == 27: #escape key
         break
+    if k == 49: #1 key
+        showFrame = not showFrame
+    if k == 50: #2 key
+        showRes = not showRes
+    if k == 51: #3 key
+        showWholeFrame= not showWholeFrame
+    if k == 52: #4 key
+        showMask = not showMask
+    if k == 53: #5 key
+        showFrameRate = not showFrameRate
     
     #clear keypoints
     keypoints[:]=[]
     rawCapture.truncate(0)
+
+    if showFrameRate == True:
+        print 'Frame rate: {0} seconds'.format(time.time()-startTime)
 
 cv2.destroyAllWindows()
